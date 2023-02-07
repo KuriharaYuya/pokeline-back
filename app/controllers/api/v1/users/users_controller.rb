01781bcc7f73f555426da6f755ea959071409a36
kurihara.sk <nikoand01@gmail.com>
2023-02-08 01:58:41 +0900

    feat: user create

--- app/controllers/api/v1/users/users_controller.rb
+++ app/controllers/api/v1/users/users_controller.rb
@@ -2,8 +2,19 @@ module Api
   module V1
     module Users
       class UsersController < ApplicationController
-        def test
-          render json: { test: "test is so good" }, status: :ok
+        require "jwt"
+
+        def create
+          user_info = JWT.decode(user_params[:access_token], nil, false).first
+          name, email, user_id, picture = user_info.values_at("name", "email", "user_id", "picture")
+          user = User.new(name:, email:, id: user_id, picture:)
+          render json: { user: }, status: :ok if user.save
+        end
+
+        private
+
+        def user_params
+          params.require(:user).permit(:access_token)
         end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :users, path: "/" do
-        get "/users/test", to: "users#test"
+        resources :users
       end
     end
   end
