70ebeab3fa0f3e27b2ee97e44f45b68d9917f567
kurihara.sk <nikoand01@gmail.com>
2023-02-08 19:29:19 +0900

    test: signup

--- app/controllers/api/v1/users/users_controller.rb
+++ app/controllers/api/v1/users/users_controller.rb
@@ -5,6 +5,9 @@ module Api
         require "jwt"
 
         def create
+          if params[:user].nil? || user_params[:access_token].nil?
+            return render json: { error: "access token is missing" }, status: :bad_request
+          end
           user_info = JWT.decode(user_params[:access_token], nil, false).first
           name, email, user_id, picture = user_info.values_at("name", "email", "user_id", "picture")
           user = User.new(name:, email:, id: user_id, picture:)
