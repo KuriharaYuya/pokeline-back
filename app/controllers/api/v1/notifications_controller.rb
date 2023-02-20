8db9343e4d80e8f94191910d65965353f797f750
kurihara.sk <nikoand01@gmail.com>
2023-02-20 23:38:55 +0900

    feat: notifications

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -5,9 +5,29 @@ module Api
       before_action :authenticate_user
 
       def index
+        # ログインしていなければ早期リターン
+        return unless current_user
+
         # ユーザーに紐づいている通知を全件取得する
-        notifications = current_user.received_notifications.sort_by(&:created_at).reverse
-        render json: { notifications: }, status: :ok
+        # notifications = current_user.received_notifications.sort_by(&:created_at).reverse
+        notifications = current_user.received_notifications.includes(:comment, :visitor, :post).sort_by(&:created_at).reverse
+        # commentのuser.nameとuser.imgを含めて返す
+        unchecks = current_user.received_notifications.where(checked: false).length
+        notifications = notifications.map do |notification|
+          visitor = notification.visitor
+          {
+            id: notification.id,
+            comment_id: notification.comment_id,
+            post_id: notification.post_id,
+            post_title: notification.post.title,
+            visitor_name: visitor.name,
+            visitor_img: visitor.picture,
+            comment_content: notification.comment.content,
+            checked: notification.checked,
+            created_at: notification.created_at,
+          }
+        end
+        render json: { notifications:, unchecks: }, status: :ok
       end
 
       def read
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,14 @@ class Notification < ApplicationRecord
   # actionを限定する
   action_list = %w[comment]
   validates :action, inclusion: { in: action_list }
+  belongs_to :comment
+  belongs_to :post
+  belongs_to :visitor, class_name: "User", foreign_key: "visitor_id"
+  validate :notification_receiver
+
+  private
+
+  def notification_receiver
+    errors.add(:visited_id, "自分自身に対して通知を送ることはできません") if visitor_id == visited_id
+  end
 end
